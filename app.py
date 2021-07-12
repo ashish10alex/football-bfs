d5b03a31f9cad7e8992463a3eb4a019713035bcb
Ashish Alex <ashish.alex10@gmail.com>
2021-07-12 01:17:18 +0100

    Security update

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask
 from flask import render_template, request
 from bfs import bfs
+
+api_key = os.environ.get('FOOTBALL_API_KEY')
 
 
 app = Flask(__name__)
@@ -16,7 +19,7 @@ def index():
 		return render_template( "index.html", connection_result_list=connection_result_list)
 
 def get_images():
-	headers = {'X-Auth-Token': 'f7a3c3dfbeb2432dbe0c65ae886c2d45'}
+	headers = {'X-Auth-Token': api_key}
 	url = 'http://api.football-data.org/v2/teams'
 	teams_data = requests.get(url, headers=headers)
 	return teams_data
